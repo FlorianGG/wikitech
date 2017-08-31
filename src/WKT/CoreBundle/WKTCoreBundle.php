230d1a433fdca0024e1e231a50a339e92764d20c
Florian Garcia <florian.prof@gmail.com>
2017-08-31 12:44:28 +0200

    Hard Reset

--- src/WKT/CoreBundle/WKTCoreBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace WKT\CoreBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class WKTCoreBundle extends Bundle
-{
-}
--- src/WKT/UserBundle/Resources/public/removeTrashLink.js
+++
@@ -1,4 +0,0 @@
-$( document ).ready(function() {
-	var trashlink = $('.trash-link');
-	trashlink.remove();
-})
