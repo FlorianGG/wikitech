07c5bc2890d0dbdbb8f90ebcff4204698c7e59b1
Florian Garcia <florian.prof@gmail.com>
2017-09-23 18:32:41 +0200

    Add redirection for no JavasScript

---
+++ web/noscript.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Wikitech : JavaScript est désactivé</title>
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}" />
+
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,600,700" rel="stylesheet">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="{{ asset('bundles/bmatznerfontawesome/css/font-awesome.min.css') }}" />
+        <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs/dt-1.10.15/r-2.1.1/datatables.min.css"/>
+    </head>
+    <body>
+     	<div class="jumbotron">
+     		<h1 class="text-center"><span class="glyphicon glyphicon-warning-sign" aria-hidden="true"> ATTENTION !</span></h1>
+     		<p class="text-center">Nous constatons que JavaScript n'est pas activé sur votre navigateur</p>
+     		<p class="text-center">Veuillez l'activer pour continuer l'aventure Wikitech</p>
+     	</div>
+    </body>
+</html>
